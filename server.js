fb332b497f5410fd4c5ee74026d374336979123c
Oprea <opreararesstefan@gmail.com>
2020-03-11 16:50:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const items = require('./routes/api/items');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +12,8 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+app.use('/api/items', items);
 
  const port = process.env.PORT || 5000;
  app.listen(port, () => console.log('Server started on port ' + port));
