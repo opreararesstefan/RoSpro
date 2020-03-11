b8a4fc949a613a08d358def00303ab051c1f4d72
Oprea <opreararesstefan@gmail.com>
2020-03-11 16:52:47 +0100

    Update items.js

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,1 +1,36 @@
+const express = require('express');
+const router = express.Router();
 
+//Item Model
+const Item = require(',,/,,/models/Item');
+
+// @route  Get api/items
+// @desc   Get All Items
+// @access Public
+route.get('/', (req, res) => {
+	Item.find()
+	.sort({ date: -1 })
+	.then(items => res.json(items));
+});
+
+// @route  Post api/items
+// @desc   Create a Item
+// @access Public
+route.get('/', (req, res) => {
+	const newItem = new Item({
+		name: req.body.name
+	});
+	
+	newItem.save().then(item => res.json(item));
+});
+
+// @route  Delete api/items
+// @desc   Delet a Item
+// @access Public
+route.delete('/:id', (req, res) => {
+	Item.findById(req.params.id)
+	  .then(item => item.remove().then(() => res.json({success: true})))
+	  .catch(err => res.status(404).jason({ success: false }));
+});
+
+module.exports = router;
