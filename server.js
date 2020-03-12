b41c3b4c690517ca9365dd4a140b68c1c6900aaf
Oprea <opreararesstefan@gmail.com>
2020-03-12 11:58:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const items = require('./routes/api/items');
 const app = express();
 
@@ -8,12 +9,23 @@ app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
 
+//Connect to Mongo
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+//user Routes
 app.use('/api/items', items);
+
+//Sere static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
  const port = process.env.PORT || 5000;
  app.listen(port, () => console.log('Server started on port ' + port));
