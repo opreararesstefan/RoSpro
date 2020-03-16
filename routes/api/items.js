a4e74748e12348c37bc0109f9719024ceaee4813
Oprea <opreararesstefan@gmail.com>
2020-03-16 13:20:18 +0100

    Update items.js

--- routes/api/items.js
+++ routes/api/items.js
@@ -30,7 +30,7 @@ router.post('/', (req, res) => {
 router.delete('/:id', (req, res) => {
 	Item.findById(req.params.id)
 	  .then(item => item.remove().then(() => res.json({success: true})))
-	  .catch(err => res.status(404).jason({ success: false }));
+	  .catch(err => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
